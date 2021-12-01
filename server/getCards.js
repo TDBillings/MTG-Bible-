0da8d387e27c6a0b477869f43fb41b8e73e3a795
Kevin Gilland <kev.gilland93@gmail.com>
2021-12-01 17:36:00 -0500

    changes to data fetch logic

--- server/getCards.js
+++ server/getCards.js
@@ -3,18 +3,43 @@ const path = require('path');
 
 const apiHost = 'http://api.scryfall.com'
 
-let getCards = (offset, limit) => {
-  let url = new URL('/cards/search/', apiHost)
-  url.searchParams.append('q', 'legal:standard')
-  url.searchParams.append('limit', limit || 0)
-  url.searchParams.append('offset', offset || 0) 
-  url.searchParams.append('order', 'usd') 
-  console.log(url.href)
-  return axios.get(url.href)
-  // .then((res) => {
-  //   console.log(res.data)
-  // })
-  .catch(err => console.log(err))
+const defaults = {
+  q:'legal:standard',
+  order:'usd'
 }
 
-module.exports = getCards
+// let getCards = (query) => {
+//   let url = new URL('/cards/search/', apiHost)
+//   url.searchParams.append('q', 'legal:standard')
+//   url.searchParams.append('limit', limit || 0)
+//   url.searchParams.append('offset', offset || 0) 
+//   url.searchParams.append('order', 'usd') 
+//   console.log(url.href)
+//   return axios.get(url.href)
+//   // .then((res) => {
+//   //   console.log(res.data)
+//   // })
+//   .catch(err => console.log(err))
+// }
+let setParams = (url, params) => {
+  Object.keys(params).forEach((key) => {
+    url.searchParams.set(key, params[key])
+  })
+  return url
+}
+
+
+let getSearchURL = (query = {}) => {
+  let url = new URL('/cards/search/', apiHost)
+  let p = Object.assign({}, defaults, query)
+  setParams(url, p)
+  return url
+}
+
+let searchCards = (query) => {
+  let url = getSearchURL(query)
+  console.log(url.href)
+  return axios.get(url.href)
+}
+
+module.exports = searchCards
--- server/index.js
+++ server/index.js
@@ -5,23 +5,34 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  let offset = 0;
-  let limit = 10;
-  getCards(offset, limit).then((data) => {
+  let offset = parseInt(req.query.offset) || 0;
+  let limit = parseInt(req.query.limit) || 10;
+
+  getCards(req.query).then((data) => {
 
     console.log(Object.keys(data))
     
     let d = data.data
-
-    d.data = d.data.filter((d, i) => i >= offset && i < offset + limit)
-
-    console.log(Object.keys(d))
+    
+    d.data = getRange(offset, offset+limit-1).map((r) => d.data[r])
     // data.data = data.data.filter((d, i) => i >= 0 && i <=10)
+    console.log(Object.keys(d))
+    
     res.json(d)
   })
+  .catch(err => console.log(err))
   // res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+function getRange(start, end) {
+  let res = []
+  for (var i = start; i <=end; i++) {
+      res.push(i)
+  }
+
+  return res
+}
